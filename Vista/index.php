2031160ebb53e76ca89b24588a01a57c14dbd7b8
Sebastian <Sebastian@192.168.1.35>
2014-09-26 03:46:21 -0300

    primera modificacion

--- Vista/index.php
+++ Vista/index.php
@@ -171,7 +171,7 @@ function MM_openBrWindow(theURL,winName,features) { //v2.0
 		
 	</head>
 	<body>
-		<h4> Inicio Sesión </h4>
+		<h4> Hola mundo </h4>
 		
 <div id="inicio" style="float:middle">
 <form name="formularioInicio" method="POST" action="recibeLogin.php" >
